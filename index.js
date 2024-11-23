40eb01a498e1cb6b630ba42264af265e6e29bc9d
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-23 21:46:32 +0300

    index modified

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import express from "express"
-import mongoose from "mongoose"
+import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
+import bookRoutes from "./routes/bookRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
-const DB = process.env.DATABASE_LOCAL ?? 'mongodb://127.0.0.1:27017/cscp-db';
+const DB = process.env.MONGO_URI ?? 'mongodb://127.0.0.1:27017/book-api';
 
 app.use(express.json({limit: '10mb'}));
 app.use(cors())
