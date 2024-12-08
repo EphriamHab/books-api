f15ab7e242ced37395edbe3b1dd786f5db5879af
EphriamHab <ephremhabtamu2015@gmail.com>
2024-12-08 15:35:00 +0300

    book  routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,19 +1,21 @@
 import express from 'express';
 
 import { getBooks, getBook, createBook, updateBook, deleteBook, getRecommendation, markFavorite, getFavoriteBooks} from '../controllers/bookControllers.js';
-
+import { protect, authorize } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 
-router.get('/recommendation', getRecommendation);
-router.get('/favorites', getFavoriteBooks);
-router.get('/', getBooks);
-router.post('/', createBook);
-router.get('/:id', getBook);
-router.put('/:id', updateBook);
-router.delete('/:id', deleteBook);
+router.get('/recommendation', protect, getRecommendation);
+router.get('/favorites', protect, getFavoriteBooks);
+router.get('/all', protect, authorize('admin'), getBooks);
+
+router.post('/', protect, authorize('user'), createBook);
+router.get('/:id', protect, getBook);
+router.put('/:id', protect, authorize('user'), updateBook);
+router.delete('/:id', protect, authorize('admin'), deleteBook);
 router.patch('/:id/markFavorite', markFavorite);
+
 
 
 export default router;
