9fa4957defd4ab06d12504571c887d15af9d1cfb
EphriamHab <ephremhabtamu2015@gmail.com>
2024-12-09 14:12:38 +0300

    auth middle ware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -17,7 +17,7 @@ export const protect = (req, res, next) => {
 export const authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Access denied" });
+      return res.status(403).json({ message: "Not autorized for this service" });
     }
     next();
   };
